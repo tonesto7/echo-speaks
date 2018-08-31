ae493e4e527d081705df0b93e4b5cc775e8d9372
tonesto7 <tonesto7@gmail.com>
2018-08-31 17:58:34 -0400

    initial upload

---
+++ devicetypes/tonesto7/echo-speaks-device.src/echo-speaks-device.groovy
@@ -0,0 +1,317 @@
+/**
+ *	Echo Speaks Device
+ *
+ *	Author: Anthony Santilli
+ ***************************
+ *
+ *  Copyright 2018 Anthony Santilli
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+import java.text.SimpleDateFormat
+String devVersion() { return "0.0.1"}
+String devModified() { return "2018-08-31"}
+String getAppImg(imgName) { return "https://raw.githubusercontent.com/tonesto7/echo-speaks/master/resources/icons/$imgName" }
+
+metadata {
+    definition (name: "Echo Speaks Device", namespace: "tonesto7", author: "Anthony Santilli") {
+        capability "Sensor"
+        capability "Refresh"
+        capability "Music Player"
+        capability "Notification"
+        capability "Speech Synthesis"
+        
+        attribute "lastUpdated", "string"
+        attribute "firmwareVer", "string"
+
+        command "sendTestTts"
+    }
+
+    preferences { 
+        input "showLogs", "bool", required: false, title: "Show Debug Logs?", defaultValue: false
+    }
+
+    tiles (scale: 2) {
+        multiAttributeTile(name: "mediaMulti", type:"mediaPlayer", width:6, height:4) {
+			tileAttribute("device.status", key: "PRIMARY_CONTROL") {
+				attributeState("paused", label:"Paused",)
+				attributeState("playing", label:"Playing")
+				attributeState("stopped", label:"Stopped")
+			}
+			tileAttribute("device.status", key: "MEDIA_STATUS") {
+				attributeState("paused", label:"Paused", action:"music Player.play", nextState: "playing")
+				attributeState("playing", label:"Playing", action:"music Player.pause", nextState: "paused")
+				attributeState("stopped", label:"Stopped", action:"music Player.play", nextState: "playing")
+			}
+			tileAttribute("device.status", key: "PREVIOUS_TRACK") {
+				attributeState("status", action:"music Player.previousTrack", defaultState: true)
+			}
+			tileAttribute("device.status", key: "NEXT_TRACK") {
+				attributeState("status", action:"music Player.nextTrack", defaultState: true)
+			}
+			tileAttribute ("device.level", key: "SLIDER_CONTROL") {
+				attributeState("level", action:"music Player.setLevel")
+			}
+			tileAttribute ("device.mute", key: "MEDIA_MUTED") {
+				attributeState("unmuted", action:"music Player.mute", nextState: "muted")
+				attributeState("muted", action:"music Player.unmute", nextState: "unmuted")
+			}
+			tileAttribute("device.trackDescription", key: "MARQUEE") {
+				attributeState("trackDescription", label:"${currentValue}", defaultState: true)
+			}
+		}
+        valueTile("blank1x1", "device.blank", height: 1, width: 1, inactiveLabel: false, decoration: "flat") {
+            state("blank1x1", label:'')
+        }
+        valueTile("blank2x1", "device.blank", height: 1, width: 2, inactiveLabel: false, decoration: "flat") {
+            state("blank1x1", label:'')
+        }
+        valueTile("firmwareVer", "device.firmwareVer", height: 1, width: 2, inactiveLabel: false, decoration: "flat") {
+            state("firmwareVer", label:'Firmware:\n${currentValue}')
+        }
+        standardTile("sendTest", "sendTest", height: 1, width: 2, decoration: "flat") {
+            state("default", label:'Send Test TTS', action: 'sendTestTts')
+        }
+        
+        main(["mediaMulti"])
+        details(["mediaMulti", "dtCreated", "firmwareVer", "sendTest"])
+    }
+}
+
+def parse(description) {
+	// No parsing will happen
+}
+
+def installed() {
+	log.trace "${device?.displayName} Executing Installed..."
+
+    state.tracks = [
+		"Gangnam Style (강남스타일)\nPSY\nPsy 6 (Six Rules), Part 1",
+		"Careless Whisper\nWham!\nMake It Big",
+		"Never Gonna Give You Up\nRick Astley\nWhenever You Need Somebody",
+		"Shake It Off\nTaylor Swift\n1989",
+		"Ironic\nAlanis Morissette\nJagged Little Pill",
+		"Hotline Bling\nDrake\nHotline Bling - Single"
+	]
+    state.currentTrack = 0
+	sendEvent(name: "level", value: 72)
+	sendEvent(name: "mute", value: "unmuted")
+	sendEvent(name: "status", value: "stopped")
+	initialize()
+}
+
+def updated() {
+	log.trace "${device?.displayName} Executing Updated..."
+	initialize()
+}
+
+def initialize() {
+	log.trace "${device?.displayName} Executing initialize"
+ 	sendEvent(name: "DeviceWatch-DeviceStatus", value: "online")
+	sendEvent(name: "DeviceWatch-Enroll", value: [protocol: "cloud", scheme:"untracked"].encodeAsJson(), displayed: false)
+}
+
+def getShortDevName(){
+    return device?.displayName?.replace("Echo - ", "")
+}
+
+def updateDeviceStatus(Map echoDevice) {
+    String devName = getShortDevName()
+    echoDevice?.each { k,v ->
+        logger("debug", "$k: $v")
+    }
+    state?.serialNumber = echoDevice?.serialNumber
+    if(echoDevice?.size()) {
+        state?.serialNumber = echoDevice?.serialNumber
+        String firmwareVer = echoDevice?.softwareVersion ?: "Not Set"
+        if(isStateChange(device, "firmwareVer", firmwareVer?.toString())) {
+            sendEvent(name: "firmwareVer", value: firmwareVer?.toString(), descriptionText: "Firmware Version is ${firmwareVer}", display: true, displayed: true)
+        }
+    }
+    setOnlineStatus((echoDevice?.monitorData?.online != false))
+    sendEvent(name: "lastUpdated", value: formatDt(new Date()), display: false , displayed: false)
+}
+
+public setOnlineStatus(Boolean isOnline) {
+    if(isStateChange(device, "DeviceWatch-DeviceStatus", (isOnline ? "online" : "offline"))) {
+        sendEvent(name: "DeviceWatch-DeviceStatus", value: (isOnline ? "online" : "offline"), displayed: true, isStateChange: true)
+    }
+}
+
+def deviceNotification(String msg) {
+    log.trace "deviceNotification(msg: $msg)"
+    if(msg) { sendTtsMsg(msg) }
+}
+
+def speak(String msg) {
+    log.trace "speak($msg)"
+    if(msg) { sendTtsMsg(msg) }
+}
+
+def play() {
+    log.debug "play() | Not Supported Yet!!!"
+	// sendEvent(name: "status", value: "playing")
+	// sendEvent(name: "trackDescription", value: state.tracks[state.currentTrack])
+}
+
+def pause() {
+    log.debug "pause() | Not Supported Yet!!!"
+	// sendEvent(name: "status", value: "paused")
+	// sendEvent(name: "trackDescription", value: state.tracks[state.currentTrack])
+}
+
+def stop() {
+    log.debug "stop() | Not Supported Yet!!!"
+	// sendEvent(name: "status", value: "stopped")
+}
+
+def previousTrack() {
+    log.debug "previousTrack() | Not Supported Yet!!!"
+	// state.currentTrack = state.currentTrack - 1
+	// if (state.currentTrack < 0) {
+	// 	state.currentTrack = state.tracks.size()-1
+    // }
+	// sendEvent(name: "trackDescription", value: state.tracks[state.currentTrack])
+}
+
+def nextTrack() {
+    log.debug "nextTrack() | Not Supported Yet!!!"
+	// state.currentTrack = state.currentTrack + 1
+	// if (state.currentTrack == state.tracks.size()) {
+	// 	state.currentTrack = 0
+    // }
+	// sendEvent(name: "trackDescription", value: state.tracks[state.currentTrack])
+}
+
+def mute() {
+    log.debug "mute() | Not Supported Yet!!!"
+	// sendEvent(name: "mute", value: "muted")
+}
+
+def unmute() {
+    log.debug "unmute() | Not Supported Yet!!!"
+	// sendEvent(name: "mute", value: "unmuted")
+}
+
+def setLevel(level) {
+    log.debug "setLevel($level) | Not Supported Yet!!!"
+	// sendEvent(name: "level", value: level)
+}
+
+def setTrack(String uri, metaData="") {
+    log.debug "setLevel($level) | Not Supported Yet!!!"
+	// log.debug "Executing 'setTrack'"
+    // sendCommand("track=$uri")
+}
+
+def resumeTrack() {
+    log.debug "resumeTrack() | Not Supported Yet!!!"
+	// log.debug "Executing 'resumeTrack'"
+	// TODO: handle 'resumeTrack' command
+}
+
+def restoreTrack() {
+    log.debug "restoreTrack() | Not Supported Yet!!!"
+	// log.debug "Executing 'restoreTrack'"
+	// TODO: handle 'restoreTrack' command
+}
+
+def playTrackAtVolume(String uri, volume) {
+    log.debug "playTrackAtVolume() | Not Supported Yet!!!"
+    // log.trace "playTrackAtVolume($uri, $volume)"
+	// sendCommand("playTrack&track=${uri}&volume=${volume}")
+}
+
+def playTrackAndResume(uri, duration, volume=null) {
+    log.debug "playTrackAndResume() | Not Supported Yet!!!"
+    
+    // log.debug "playTrackAndResume($uri, $duration, $volume)"
+	// def cmd = "playTrack&track=${uri}&resume"
+	// if (volume) {
+	// 	cmd += "&volume=${volume}"
+    // }
+    // sendCommand(cmd)
+}
+
+def playTextAndResume(text, volume=null) {
+    log.debug "playTextAndResume($text, $volume)"
+    // def sound = textToSpeech(text)
+    // playTrackAndResume(sound.uri, (sound.duration as Integer) + 1, volume)
+    if(text) { sendTtsMsg(text) }
+}
+
+def playTrackAndRestore(uri, duration, volume=null) {
+    log.debug "playTrackAndRestore() | Not Supported Yet!!!"
+    // log.debug "playTrackAndResume($uri, $duration, $volume)"
+	// def cmd = "playTrack&track=${uri}&restore"
+	// if (volume) {
+	// 	cmd += "&volume=${volume}"
+    // }
+    // sendCommand(cmd)
+}
+
+def playTextAndRestore(text, volume=null) {
+    log.debug "playTextAndResume($text, $volume)"
+	// def sound = textToSpeech(text)
+	// playTrackAndRestore(sound.uri, (sound.duration as Integer) + 1, volume)
+    if(text) { sendTtsMsg(text) }
+}
+
+def playURL(theURL) {
+    log.debug "playURL() | Not Supported Yet!!!"
+	// log.debug "Executing 'playURL'"
+    // sendCommand("url=$theURL")
+}
+
+def playSoundAndTrack(soundUri, duration, trackData, volume=null) {
+    log.debug "playSoundAndTrack() | Not Supported Yet!!!"
+	// log.debug "playSoundAndTrack($uri, $duration, $trackData, $volume)"
+	// def cmd = "playTrack&track=${soundUri}&playlist=${trackData.station}"
+	// if (volume) {
+	// 	cmd += "&volume=${volume}"
+    // }
+    // sendCommand(cmd)
+}
+
+def sendTestTts(ttsMsg) {
+    log.trace "sendTestTts"
+    if(!ttsMsg) { ttsMsg = "Testing Testing 1, 2, 3"}
+	sendTtsMsg(ttsMsg)
+}
+
+public sendTtsMsg(String msg) {
+    if(state?.serialNumber && msg) {
+		parent?.echoServiceCmd("tts", [deviceSerialNumber: state?.serialNumber, tts: msg])
+	} else { log.warn "sendTtsMsg Error | You are missing one of the following... SerialNumber: ${state?.serialNumber} or Message: ${msg}" }
+}
+
+/*****************************************************
+                HELPER FUNCTIONS
+******************************************************/
+def formatDt(dt, String tzFmt=("MM/d/yyyy hh:mm:ss a")) {
+	def tf = new SimpleDateFormat(tzFmt); tf.setTimeZone(location.timeZone);
+    return tf.format(dt)
+}
+
+def parseDt(dt, dtFmt) {
+    return Date.parse(dtFmt, dt)
+}
+
+Boolean ok2Notify() {
+    return (parent?.getOk2Notify())
+}
+
+private logger(type, msg) {
+	if(type && msg && settings?.showLogs) {
+		log."${type}" "${msg}"
+	}
+}
---
+++ echo_speaks_utility.sh
@@ -0,0 +1,387 @@
+#!/bin/bash
+
+# ---------------------------------GLOBAL VARIABLES--------------------------------------
+_scriptVer="0.1"
+_srvcVer="0.0.1"
+_useSudo="false"
+_instNode="false"
+_port="8091"
+
+_currentUser="$USER"
+
+_userDir="/home/$_currentUser"
+working_app_dir="$_userDir/echo-speaks-master"
+
+src_zip_name="master.zip"
+app_zip_file="$_userDir/$src_zip_name"
+
+cur_srvc_name="echo-speaks.service"
+new_srvc_src_path="$working_app_dir/node_server/$cur_srvc_name"
+new_srvc_dest_path="/etc/systemd/system/$cur_srvc_name"
+
+old_srvc_name="echo-speaks.service"
+old_srvc_path="/etc/systemd/system/$old_srvc_name"
+
+remote_file="https://github.com/tonesto7/echo-speaks/archive/$src_zip_name"
+# ----------------------------------------------------------------------------------------
+
+showTitle() {
+    echo "    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
+    echo "    ┃                Echo Speaks Utility Script (v$_scriptVer)                      ┃"
+    echo "    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"
+}
+
+showHelp() {
+    echo "    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
+    echo "    ┃                Echo Speaks Utility Script (v$_scriptVer)                      ┃"
+    echo "    ┃                            Help Page                                  ┃"
+    echo "    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"
+    echo ""
+    echo "      Available Switch Arguments: "
+    echo "    ┌──────────────────────────┬────────────────────────────────────────────┐"
+    echo "    │  Default [No Arg]        │ Runs the Full Installation Process         │"
+    echo "    │                          │                                            │"
+    echo "    │  [-port $_port]            │ Defines custom port - Default: ($_port)      │"
+    echo "    │                          │                                            │"
+    echo "    │  [-f | -force]           │ Forcefully Update Files/Service            │"
+    echo "    │                          │                                            │"
+    echo "    │  [-r | -remove | -clean] │ Removes the Service and All Echo Speaks    │"
+    echo "    │                          │ files from the System                      │"
+    echo "    │                          │                                            │"
+    echo "    │  [-u | -update]          │ Skip's Pre-req Install and downloads the   │"
+    echo "    │                          │ latest package file and updates the        │"
+    echo "    │                          │ existing files and reinstalls the service  │"
+    echo "    │                          │                                            │"
+    echo "    ├──────────────────────────┴────────────────────────────────────────────┤"
+    echo "    │       You can use the -port parameter by itself or after -update      │"
+    echo "    │       and -force                                                      │"
+    echo "    └───────────────────────────────────────────────────────────────────────┘"
+    echo ""
+    echo ""
+    exit
+}
+
+#   Checks to see if sudo should be used by default.
+#   It allows an argument of "true"/"false" to use sudo if it's allowed/supported on the machine
+check_sudo() {
+    _useSudo="false"
+    if [ -x "$(command -v sudo)" ]; then
+        if [ "$1" = "true" ]; then
+            if [ $_currentUser != "root" ]; then
+                _useSudo="true"
+            fi
+        fi
+    fi
+    #echo "Using Sudo: ($_useSudo)"
+}
+
+verifySystemd() {
+    if [ ! -x "$(command -v systemctl)" ]; then
+        echo "Error: Systemd is not available on this device..."
+        exit 1
+    fi
+}
+
+checkOwnerOk() {
+    dir_owner="$(stat -c '%U' $_userDir)"
+    if [ $_currentUser != $dir_owner ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+usedSudoDesc() {
+    if $_useSudo; then
+        return " | (SUDO)"
+    else
+        return ""
+    fi
+}
+
+sudoPreCmd() {
+    _cmd=$1
+    if $_useSudo; then
+        _cmd="sudo $_cmd"; $_cmd
+    else
+        $_cmd
+    fi
+}
+
+#   Set current user as owner of apps working directory
+set_owner() {
+    echo ""
+    echo "--------------------------------------------------------------"
+    check_sudo "true"
+    echo "Making ($_currentUser) Owner of directory [$_userDir]$usedSudoDesc"
+    sudoPreCmd "chown -R $_currentUser:$_currentUser $_userDir"
+    # verifies owner was set
+    if [ ! checkOwnerOk ]; then
+        echo "Error: Setting $_currentUser failed for Directory ($_userDir)"
+        exit 1
+    fi
+}
+
+#   This installs all pre-requisite APT Packages required by the service
+install_prereqs() {
+    echo ""
+    echo "--------------------------------------------------------------"
+    check_sudo "true"
+    echo "Installing/Updating Required APT Packages$usedSudoDesc"
+    sudoPreCmd "apt-get update"
+    sudoPreCmd "apt-get upgrade -f -y --force-yes"
+    sudoPreCmd "apt-get install curl wget git-core unzip openssh-server build-essential -y"
+    if $_instNode; then
+        if $_useSudo; then
+            curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+        else
+            curl -sL https://deb.nodesource.com/setup_10.x | bash -
+        fi
+        sudoPreCmd "apt-get install nodejs -y"
+    fi
+}
+
+getLatestPackage() {
+    download_package
+    if [ -f $app_zip_file ];
+    then
+        remove_old_srvc
+        unzip_pkg
+        #check_sudo # because set_owner changes the sudo allow to true
+        echo "Changing to [$working_app_dir] directory..."
+        cd $working_app_dir
+        install_node_app
+        update_srvc
+        remove_package
+        exit 0
+    else
+        echo "Error: Echo Speaks Package Zip File ($app_zip_file) Not Found"
+        echo "at ($_userDir)"
+        exit 1
+    fi
+}
+
+#   This Downloads the latest NST Streaming package from the distribution source and extracts it
+download_package() {
+    if [ checkOwnerOk != "true" ]; then
+        set_owner
+    fi
+    check_sudo
+    echo ""
+    echo "--------------------------------------------------------------------------"
+    echo "Downloading the Latest Echo Speaks Package ($src_zip_name)$usedSudoDesc"
+    echo "--------------------------------------------------------------------------"
+    sudoPreCmd "wget -N $remote_file -P $_userDir"
+}
+
+remove_package() {
+    if [ checkOwnerOk != "true" ]; then
+        set_owner
+    fi
+    check_sudo
+    echo ""
+    echo "--------------------------------------------------------------------------"
+    echo "Cleaning Up Downloaded Package ($app_zip_file)$usedSudoDesc"
+    echo "--------------------------------------------------------------------------"
+    sudoPreCmd "rm -rf $app_zip_file"
+}
+
+unzip_pkg() {
+    echo ""
+    echo "--------------------------------------------------------------"
+    check_sudo
+    if [ -f $app_zip_file ];
+    then
+        cd $_userDir
+        echo "Unzipping Latest Echo Speaks Package to $working_app_dir$usedSudoDesc"
+        sudoPreCmd "unzip -o $app_zip_file"
+        set_owner
+    else
+        echo "Error: Unzip Failed because $app_zip_file can't be located"
+        exit 1
+    fi
+}
+
+install_node_app() {
+    echo ""
+    echo "--------------------------------------------------------------"
+    check_sudo
+    if [ -d $working_app_dir/node_server ]; then
+        cd $working_app_dir/node_server
+        echo "Running Node Service Install$usedSudoDesc"
+        sudoPreCmd "npm install --no-optional"
+    else
+        echo "Error: Node App NPM Install Failed because the directory ($working_app_dir/node_server) wasn't found"
+        exit 1
+    fi
+}
+
+modify_srvc_file_for_user() {
+    check_sudo
+    #echo "New NST Streaming Service File found. Updating!!!"
+    if [ $_currentUser != "pi" ];
+    then
+        echo "Modifying Service File with Current User Path [$_userDir]$usedSudoDesc"
+        sudoPreCmd "sed -ia 's|/home/pi/echo-speaks-master|'$working_app_dir/node_server'|g' $cur_srvc_name"
+    fi
+}
+
+create_srvc_file_for_user() {
+    echo "Creating Service File with Current User Path [$_userDir]$usedSudoDesc"
+    echo "[Unit]" >> $new_srvc_src_path
+    echo "Description=Echo Speaks Node Server" >> $new_srvc_src_path
+    echo "After=network.target remote-fs.target" >> $new_srvc_src_path
+    echo "" >> $new_srvc_src_path
+    echo "[Service]" >> $new_srvc_src_path
+    echo "Type=simple" >> $new_srvc_src_path
+    # echo "Environment=NODE_ENV=production" >> $new_srvc_src_path
+    echo "Environment=ECHO_SPEAKS_PORT=$_port" >> $new_srvc_src_path
+    echo "ExecStart=/usr/bin/node index.js" >> $new_srvc_src_path
+    echo "Restart=always" >> $new_srvc_src_path
+    echo "RestartSec=10" >> $new_srvc_src_path
+    echo "KillMode=process" >> $new_srvc_src_path
+    echo "WorkingDirectory=$working_app_dir/node_server" >> $new_srvc_src_path
+    echo "" >> $new_srvc_src_path
+    echo "[Install]" >> $new_srvc_src_path
+    echo "WantedBy=multi-user.target" >> $new_srvc_src_path
+}
+
+remove_old_srvc() {
+    echo ""
+    echo "--------------------------------------------------------------"
+    echo "Checking for Existing Echo Speaks Service File"
+    if [ -f $old_srvc_path ]; then
+        check_sudo "true"
+        echo "Result: Found Existing Service File..."
+        echo "Disabling and Removing Existing Echo Speaks Service$usedSudoDesc"
+        sudoPreCmd "systemctl stop $old_srvc_name"
+        sudoPreCmd "systemctl disable $old_srvc_name"
+        sudoPreCmd "rm $old_srvc_path"
+    else
+        echo "Result: Nothing to Remove.  No Existing Service File Found"
+    fi
+}
+
+update_srvc() {
+    echo ""
+    echo "--------------------------------------------------------------"
+    create_srvc_file_for_user
+    if [ ! -f "$new_srvc_src_path" ]; then
+        echo "Error: The created service file cannot be found"
+        exit 1
+    fi
+    if [ ! -f "$old_srvc_path" ]; then
+        check_sudo "true"
+        echo "Copying Updated Echo Speaks Service File to Systemd Folder$usedSudoDesc"
+        sudoPreCmd "cp $new_srvc_src_path $new_srvc_dest_path -f"
+        if [ -f $new_srvc_dest_path ]; then
+            echo "Reloading Systemd Daemon to Reflect Service File Changes$usedSudoDesc"
+            sudoPreCmd "systemctl daemon-reload"
+            echo "Enabling Echo Speaks Service (Systemd)$usedSudoDesc"
+            sudoPreCmd "systemctl enable $cur_srvc_name"
+            echo "Starting Echo Speaks Streaming Service (v$_srvcVer)$usedSudoDesc"
+            sudoPreCmd "systemctl start $cur_srvc_name"
+        else
+            echo "Error: Copying Service File to Systemd folder didn't work"
+            exit 1
+        fi
+    else
+        echo "Error: Can't Copy New Service because Old File is Still There!!"
+        exit 1
+    fi
+}
+
+pkg_cleanup() {
+    check_sudo
+    if [ -d $working_app_dir ]; then
+        echo ""
+        echo "----------------------------------------------------------------------"
+        if [ checkOwnerOk != "true" ]; then
+            set_owner
+        fi
+        echo "Removing All Echo Speaks Data and Files$usedSudoDesc"
+        sudoPreCmd "rm -rf $working_app_dir"
+        if [ -f $_userDir/$src_zip_name ]; then
+            sudoPreCmd "rm -rf $_userDir/$src_zip_name"
+        fi
+    fi
+}
+
+setPortVariable() {
+    _val=$1
+    ECHO_SPEAKS_PORT=$_val
+    echo "ECHO_SPEAKS_PORT variable is: $ECHO_SPEAKS_PORT";
+}
+
+remove_all() {
+    remove_old_srvc
+    unset ECHO_SPEAKS_PORT
+    pkg_cleanup
+}
+
+showPkgDlOk() {
+    echo ""
+    echo "=============================================================="
+    echo "               Install/Update Result: (Success)               "
+    echo "          New Data Downloaded and Service Installed           "
+    echo "                                                              "
+    echo "            View Service Logs Using this Command:             "
+    echo "               journalctl -f -u echo-speaks                 "
+    echo "=============================================================="
+    echo ""
+}
+
+showCleanupOk() {
+    echo ""
+    echo "=============================================================="
+    echo "              Cleanup/Removal Result: (Success)               "
+    echo "        All Echo Speaks Data, Files, Service Removed"
+    echo "=============================================================="
+    echo ""
+}
+
+# echo "Executing Script $0 $1"
+clear
+verifySystemd
+if [ $# -eq 0 ]; then
+    showTitle
+    install_prereqs
+    getLatestPackage
+    
+else
+    if [ "$1" = "-p" ] || [ "$1" = "-port" ]; then
+        _port="$2"
+        setPortVariable "$_port"
+        showTitle
+        install_prereqs
+        getLatestPackage
+    elif [ "$1" = "-r" ] || [ "$1" = "-remove" ] || [ "$1" = "-clean" ]; then
+        showTitle
+        remove_all
+        showCleanupOk
+
+    elif [ "$1" = "-f" ] || [ "$1" = "-force" ]; then
+        if [ "$2" = "-p" ] || [ "$2" = "-port" ]; then
+            _port="$3"
+        fi
+        setPortVariable "$_port"
+        showTitle
+        remove_all
+        getLatestPackage
+        showPkgDlOk
+        sudoPreCmd "journalctl -f -u echo-speaks.service"
+
+    elif [ "$1" = "-u" ] || [ "$1" = "-update" ]; then
+        if [ "$2" = "-p" ] || [ "$2" = "-port" ]; then
+            _port="$3"
+        fi
+        setPortVariable "$_port"
+        showTitle
+        getLatestPackage
+        showPkgDlOk
+        sudoPreCmd "journalctl -f -u echo-speaks.service"
+
+    elif [ "$1" = "-help" ] || [ "$1" = "-h" ] || [ "$1" = "-?" ] || [ "$1" != "-u" ] || [ "$1" != "-update" ] || [ "$1" != "-f" ] || [ "$1" != "-force" ] || [ "$1" != "-r" ] || [ "$1" != "-remove" ] || [ "$1" != "-clean" ]; then
+        showHelp
+    fi
+fi
