cebf7870f2f06207f509b2f6b769f3f61809ae4c
Tonesto7 <tonesto7@gmail.com>
2018-10-31 23:59:52 -0400

    updated version info

--- devicetypes/tonesto7/echo-speaks-device.src/echo-speaks-device.groovy
+++ devicetypes/tonesto7/echo-speaks-device.src/echo-speaks-device.groovy
@@ -16,8 +16,8 @@
 
 import java.text.SimpleDateFormat
 include 'asynchttp_v1'
-String devVersion() { return "1.0.1"}
-String devModified() { return "2018-10-31"}
+String devVersion() { return "1.0.2"}
+String devModified() { return "2018-11-01"}
 String getAppImg(imgName) { return "https://raw.githubusercontent.com/tonesto7/echo-speaks/master/resources/icons/$imgName" }
 
 metadata {
