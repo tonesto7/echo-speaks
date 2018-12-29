ae70b1ee7381de644cc9724b5c52fbbe768ea527
Tony Fleisher <tony.fleisher@gmail.com>
2018-12-29 11:36:15 -0800

    Feature: TTS announcements to all devices

--- devicetypes/tonesto7/echo-speaks-device.src/echo-speaks-device.groovy
+++ devicetypes/tonesto7/echo-speaks-device.src/echo-speaks-device.groovy
@@ -61,6 +61,7 @@ metadata {
         command "playTrackAndRestore"
         command "playTextAndRestore"
         command "sendTestTts"
+        command "sendTestAnnouncement"
         command "replayText"
         command "doNotDisturbOn"
         command "doNotDisturbOff"
@@ -73,6 +74,7 @@ metadata {
         command "playGoodMorning"
         command "playTraffic"
         command "playTellStory"
+        command "playAnnouncement"
         command "searchMusic"
         command "searchAmazonMusic"
         command "searchAppleMusic"
@@ -249,6 +251,9 @@ metadata {
         standardTile("sendTest", "sendTest", height: 1, width: 2, decoration: "flat") {
             state("default", label:'Send Test TTS', action: 'sendTestTts')
         }
+        standardTile("sendTestAnnouncement", "sendTestAnnouncement", height: 1, width: 2, decoration: "flat") {
+            state("default", label:'Send Test Announcement', action: 'sendTestAnnouncement')
+        }        
         standardTile("playWeather", "playWeather", height: 1, width: 2, decoration: "flat") {
             state("default", label:'Weather Report', action: 'playWeather')
         }
@@ -283,7 +288,7 @@ metadata {
         main(["deviceStatus"])
         details([
             "mediaMulti", "currentAlbum", "currentStation", "dtCreated", "deviceFamily", "deviceStyle", "onlineStatus", "alarmVolume", "volumeSupported", "alexaWakeWord", "ttsSupported",
-            "playWeather", "playSingASong", "playFlashBrief", "playGoodMorning", "playTraffic", "playTellStory", "sendTest", "doNotDisturb", "resetQueue", "refresh", "supportedMusic",
+            "playWeather", "playSingASong", "playFlashBrief", "playGoodMorning", "playTraffic", "playTellStory", "sendTest", "sendTestAnnouncement", "doNotDisturb", "resetQueue", "refresh", "supportedMusic",
             "lastSpeakCmd", "lastCmdSentDt"])
     }
 
@@ -1276,6 +1281,10 @@ def playTellStory(volume=null, restoreVolume=null) {
     incrementCntByKey("use_cnt_playStory")
 }
 
+def playAnnouncement(String text) { 
+    doSequenceCmd("Announcement", "announcement", text)
+}
+
 def searchMusic(String searchPhrase, String providerId, volume=null) {
     if(musicSearchSupported()) {
         doSearchMusicCmd(searchPhrase, providerId, volume)
@@ -1540,6 +1549,10 @@ def sendTestTts(ttsMsg) {
     ]
     if(!ttsMsg) { ttsMsg = getRandomItem(items) }
     speak(ttsMsg as String)
+}
+
+def sendTestAnnouncement() {
+    playAnnouncement("Test announcement from device")
 }
 
 /*******************************************************************
@@ -1981,6 +1994,30 @@ Map createSequenceNode(command, value) {
             case "speak":
                 seqNode?.type = "Alexa.Speak"
                 seqNode?.operationPayload?.textToSpeak = value as String
+                break
+            case "announcement":
+                seqNode?.type = "AlexaAnnouncement"
+                Map payload = seqNode?.operationPayload
+                payload?.remove('deviceType')
+                payload?.remove('deviceSerialNumber')
+                payload?.remove('locale')
+                payload?.expireAfter = "PT5S"
+                List payloadContent = []
+                Map payloadContentItem = [
+                    "locale": "en-US",
+                    "display": [ 
+                        "title": "Alexa Announcements",
+                        "body": value
+                    ],
+                    "speak": [
+                        "type": "text",
+                        "value": value
+                    ]
+                ]
+                payloadContent.push(payloadContentItem)
+                payload?.content = payloadContent
+                Map payloadTarget = [ "customerId" : state?.deviceOwnerCustomerId ]
+                payload?.target = payloadTarget
                 break
             default:
                 return
