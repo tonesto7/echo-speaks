a085049010f40000558a1d71dbf58611d50e5f91
Tonesto7 <tonesto7@gmail.com>
2019-09-16 21:12:51 -0400

    diag updates

--- smartapps/tonesto7/echo-speaks.src/echo-speaks.groovy
+++ smartapps/tonesto7/echo-speaks.src/echo-speaks.groovy
@@ -3023,6 +3023,10 @@ private getDiagDataJson() {
                 lastServerWakeDur: state?.lastServerWakeDt ? seconds2Duration(getLastServerWakeSec()) : null,
                 serverPlatform: state?.onHeroku ? "Cloud" : "Local",
                 randomName: state?.generatedHerokuName
+            ],
+            versionChecks: [
+                minVersionUpdates: getMinVerUpdsRequired(),
+                updateItemsOther: codeUpdateItems()
             ]
         ]
         def json = new groovy.json.JsonOutput().toJson(output)
