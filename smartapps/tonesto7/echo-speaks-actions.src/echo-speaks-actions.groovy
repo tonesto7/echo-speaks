b3aa60ba76ec3c741b8ec422661e26892ebb01ab
tonesto7 <tonesto7@gmail.com>
2020-03-06 12:06:25 -0500

    v3.6.1.0

--- smartapps/tonesto7/echo-speaks-actions.src/echo-speaks-actions.groovy
+++ smartapps/tonesto7/echo-speaks-actions.src/echo-speaks-actions.groovy
@@ -14,8 +14,8 @@
  *
  */
 
-String appVersion()  { return "3.6.0.0" }
-String appModified() { return "2020-03-03" }
+String appVersion()  { return "3.6.1.0" }
+String appModified() { return "2020-03-06" }
 String appAuthor()   { return "Anthony S." }
 Boolean isBeta()     { return false }
 Boolean isST()       { return (getPlatform() == "SmartThings") }
