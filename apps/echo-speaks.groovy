265677ce4074ea3800cffe32a223ab093830f33d
imnotbob <Imnot_bob@hotmail.com>
2021-03-12 08:29:27 -0500

    fix caller (#724)

--- apps/echo-speaks.groovy
+++ apps/echo-speaks.groovy
@@ -3661,7 +3661,7 @@ void sendDevObjCmd(List<Map> odevObj, String myCmd, String title, String newmsg,
                     queueMultiSequenceCommand(mainSeq, myMsg)
                     devObj.each { dev-> 
                         def child = getChildDevice((String)dev.dni)
-                        child.finishAnnounce(origMsg)
+                        child.finishAnnounce(origMsg, volume, restoreVolume)
                     }
                 }
 
