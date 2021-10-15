b345a73a0c7436eef201d2a0a830439aaf13870d
imnotbob <Imnot_bob@hotmail.com>
2021-10-15 16:05:32 -0400

    fix (#943)
    * fix
    
    * Update echo-speaks-ws.groovy

--- drivers/echo-speaks-ws.groovy
+++ drivers/echo-speaks-ws.groovy
@@ -145,9 +145,9 @@ def connect() {
     try {
         Map macDms = getMacDmsVal()
         log.debug "macDms: ${macDms}"
-        def rs = getRS(macDms)
+        String rs = getRS(macDms)
 
-        log.debug "RS: ${getObjType(rs)} ${rs.toString()}"
+        log.debug "RS: ${getObjType(rs)} ${rs}"
         Map<String,Object> headers = [
             "Connection": "keep-alive, Upgrade",
             "Upgrade": "websocket",
@@ -178,7 +178,7 @@ def connect() {
 
 @Field static final String sAPPJSON       = 'application/json'
 
-def getRS(Map macDms) {
+String getRS(Map macDms) {
     String meth='getRS'
     Map params = [
         uri: (String)parent.getServerHostURL(),
@@ -194,7 +194,7 @@ def getRS(Map macDms) {
                 def sData = resp?.data
                 log.trace("getRS Data  ${sData}")
                 log.trace("getRS Data (${getObjType(sData)}): ${sData?.rs}")
-                return sData
+                return sData.rs
             }
         }
     } catch (ex) {
