3253b895b951e6edb32ea715a965aef4f4a0e2ff
Anthony S <tonesto7@gmail.com>
2022-04-18 09:16:31 -0400

    v4.2.0.2

--- drivers/echo-speaks-device.groovy
+++ drivers/echo-speaks-device.groovy
@@ -25,8 +25,8 @@ import java.text.SimpleDateFormat
 //************************************************
 //*               STATIC VARIABLES               *
 //************************************************
-@Field static final String devVersionFLD  = '4.2.0.1'
-@Field static final String devModifiedFLD = '2022-04-14'
+@Field static final String devVersionFLD  = '4.2.0.2'
+@Field static final String devModifiedFLD = '2022-04-18'
 @Field static final String sNULL          = (String)null
 @Field static final String sBLANK         = ''
 @Field static final String sSPACE         = ' '
